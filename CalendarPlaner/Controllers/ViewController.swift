eb34097db70f89014ac9a074ae6fc7cb9d78b64f
shoot21 <nikitaivashenkov01@icloud.com>
2021-11-10 19:45:30 +0300

    Add hierarchy

---
+++ CalendarPlaner/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalendarPlaner
+//
+//  Created by Никита Иващенков on 10/11/2021.
+//  Copyright © 2021 Никита Иващенков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
+
